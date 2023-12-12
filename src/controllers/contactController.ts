a0e9608565e6c7e483d10027ca96c864c70b14c5
yana-glt <galitsynayasya@gmail.com>
2023-12-12 15:53:06 +0100

    fix: replace any type with CustomRequest type for contactController

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -1,13 +1,14 @@
 import express, { Router, Request, Response } from "express";
 import Contact from "../models/contact";
+import CustomRequest from '../middleware/customRequest';
 
 class ContactController {
-  public static getContact = async (req: any, res: Response) => {
+  public static getContact = async (req: CustomRequest, res: Response) => {
     const user = req.user;
     res.render("contact/index", { user: user });
   };
 
-  public static saveContact = async (req: any, res: Response) => {
+  public static saveContact = async (req: CustomRequest, res: Response) => {
     const contact = new Contact({
       name: req.body.name,
       email: req.body.email,
